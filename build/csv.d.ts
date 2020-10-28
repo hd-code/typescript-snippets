d7f98b7a5f8923cf7cbb92539f1aad30379d0531
Hannes Dröse <hd@HDsMacBookPro.local>
2020-10-28 22:21:45 +0100

    removed all old stuff

--- build/csv.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function serialize<T>(data: T[]): string;
-export declare function parse<T>(csv: string): T[];
--- build/dom.js
+++
@@ -1,1 +0,0 @@
-"use strict";function $(e){return document.querySelectorAll(e)}function createElement(e,t,n){if(!e)return null;var r=document.createElement(e);for(var i in t)r.setAttribute(i,t[i]);return r.innerHTML=n||"",r}function insertBefore(e,t){t.parentElement.insertBefore(e,t)}function insertAfter(e,t){t.parentElement.insertBefore(e,t.nextElementSibling)}
