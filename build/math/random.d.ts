b7d27e91d6df386a56dde3af39844630e0fe74cc
Hannes Dröse <hd@HDsMacBookPro.local>
2020-10-29 00:16:15 +0100

    added random

---
+++ build/math/random.d.ts
@@ -0,0 +1,31 @@
+/*! random v0.0.1 | MIT | © Hannes Dröse https://github.com/hd-code/js-snippets */
+/**
+ * @file
+ * The JavaScript Math.random() function is not seedable. This package provides
+ * an implementation of the Lehmer random number generator. The generator is
+ * seedable, but will use a random seed when none was set.
+ *
+* Make sure to set the seed only once in your application, ideally on startup.
+*/
+/** Returns a random number between 0 (included) and 1 (not included). */
+export declare function getFloat(): number;
+/** Returns a random number between 0 and `max` (both included). */
+export declare function getFloat(max: number): number;
+/** Returns a random number between `min` and `max` (both included). */
+export declare function getFloat(min: number, max: number): number;
+/** The maximum integer that is returned by the `getInt()` function. */
+export declare const MAX_INT: number;
+/** Returns an integer between 0 and `MAX_INT` (both included). */
+export declare function getInt(): number;
+/** Returns an integer between 0 and `max` (both included).
+ *
+ * _Note:_ Decimals will be ignored.
+ */
+export declare function getInt(max: number): number;
+/** Returns an integer between `min` and `max` (both included).
+ *
+ * _Note:_ Decimals will be ignored.
+ */
+export declare function getInt(min: number, max: number): number;
+/** Sets the seed for the random number generator to 0. */
+export declare function setSeed(seed?: number): void;
