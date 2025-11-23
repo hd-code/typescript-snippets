b53061bdfcf6071f646c463a3fd1fd3f6d4ceef3
Hannes Dröse <54773293+hd-code@users.noreply.github.com>
2025-11-23 15:52:08 +0100

    added test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  collectCoverage: true,
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
